5f44883de289aa2c174d4445301a0c5f48bfbf46
YoruSamurai <3191393205@qq.com>
2025-03-17 00:42:59 +0800

    添加了视差效果，优化了攀爬的瞬移 改为dotween缓慢移动

---
+++ Necromancer/Assets/Scripts/Other/Background/ParallaxBackground.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private GameObject cineCamera;
+
+    [SerializeField] private float parallaxEffect;
+    [SerializeField] private int index;
+
+    private float xPosition;
+
+    private void Start()
+    {
+        xPosition = transform.position.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float distanceToMove = cineCamera.transform.position.x * parallaxEffect;
+        if(index ==1)
+            transform.position = new Vector3(xPosition + distanceToMove, cineCamera.transform.position.y);
+        if(index == 2)
+            transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+    }
+}
--- Necromancer/Assets/Scripts/Player/PlayerAnimationTriggers.cs
+++ Necromancer/Assets/Scripts/Player/PlayerAnimationTriggers.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,14 +35,30 @@ public class PlayerAnimationTriggers : MonoBehaviour
 
     private void LedgeUpOffsetTrigger()
     {
-        // 获取当前玩家位置
+        /*// 获取当前玩家位置
         Vector2 newPosition = player.transform.position;
         // 计算新的位置
         newPosition.x += player.facingDir * 2f; // 向面向方向移动 2f
         newPosition.y += 2f; // 高度增加 2f
 
         // 应用新位置
-        player.transform.position = newPosition;
+        player.transform.position = newPosition;*/
+
+        // 获取当前玩家位置
+        Vector2 startPosition = player.transform.position;
+        Vector2 targetPosition = new Vector2(
+            startPosition.x + player.facingDir * 2f,
+            startPosition.y + 2f
+        );
+
+        // 使用DoTween平滑移动
+        player.transform.DOMove(targetPosition, 0.08f) // 0.5秒完成位移
+            .SetEase(Ease.OutQuad) // 设置缓动效果，OutQuad是先快后慢
+            .OnComplete(() =>
+            {
+                // 移动结束后的回调，可以处理后续状态
+                //player.AnimationTrigger();
+            });
     }
 
     //只是一个trigger 触发
